71a4e8c4b8b27353c1890b8fa868e461d93059eb
Jan Nanista <jan.nanista@gooddata.com>
2015-03-18 16:55:15 +0100

    Added whole functionality and tests

---
+++ src/gizoogle.js
@@ -0,0 +1,82 @@
+'use strict';
+
+var cheerio = require('cheerio');
+var http = require('http');
+var querystring = require('querystring');
+
+var HOSTNAME = 'gizoogle.net';
+
+function createRequest(path, payload, callback) {
+    return http.request({
+        protocol: 'http',
+        hostname: HOSTNAME,
+        port: 80,
+        path: path,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': Buffer.byteLength(payload, 'utf8')
+        }
+    }, callback)
+}
+
+function parseResponse(res, callback) {
+    var chunks = [];
+
+    res.on('data', function (chunk) {
+        chunks.push(chunk);
+    });
+
+    res.on('end', function () {
+        callback(Buffer.concat(chunks).toString('utf8'));
+    });
+}
+
+module.exports = {
+    string: function(string, callback) {
+        var payload = querystring.stringify({ translatetext: string });
+        var req = createRequest('/textilizer.php', payload, function receiveHTTPResponse(res) {
+            parseResponse(res, function extractTranslatedString(body) {
+                if (res.statusCode !== 200) {
+                    return callback(Error('Got error response HTTP ' + res.statusCode));
+                }
+
+                var html = cheerio.load(body.toString('utf8')),
+                    text = html('textarea[name="translatetext"]').val();
+
+                if (!text) {
+                    return callback(new Error('Got empty response'));
+                }
+
+                callback(null, text.trim());
+            });
+        });
+
+        req.on('error', callback);
+
+        req.write(payload, 'utf8');
+        req.end();
+
+        return req;
+    },
+
+    website: function(url, callback) {
+        var payload = querystring.stringify({ search: url });
+        var req = createRequest('/tranzizzle.php', payload, function receiveHTTPResponse(res) {
+            parseResponse(res, function extractTranslatedString(body) {
+                if (res.statusCode !== 200) {
+                    return callback(Error('Got error response HTTP ' + res.statusCode));
+                }
+
+                callback(null, body);
+            });
+        });
+
+        req.on('error', callback);
+
+        req.write(payload, 'utf8');
+        req.end();
+
+        return req;
+    }
+}
--- test/gizoogle_test.js
+++ test/gizoogle_test.js
@@ -1,5 +1,128 @@
 'use strict';
 
-describe('Gizoogle', function() {
+var expect = require('expect.js');
+var fs = require('fs');
+var nock = require('nock');
+var G = require('../src/gizoogle.js');
 
+describe('Gizoogle', function() {
+    beforeEach(function() {
+        this.mock = nock('http://gizoogle.net');
+    });
+
+    afterEach(function() {
+        delete this.mock;
+    });
+
+    describe('string()', function() {
+        beforeEach(function() {
+            this.post = this.mock.post('/textilizer.php', {
+                translatetext: 'hello world'
+            });
+        });
+
+        afterEach(function() {
+            delete this.post;
+        });
+
+        context('when API call succeeds', function() {
+            before(function() {
+                this.responseText = fs.readFileSync(__dirname + '/mocks/textilizer_success.html');
+            });
+
+            after(function() {
+                delete this.responseText;
+            });
+
+            beforeEach(function() {
+                this.post.reply(200, this.responseText);
+            });
+
+            it('should pass null as error', function(done) {
+                G.string('hello world', function(error, translation) {
+                    expect(error).to.be(null);
+
+                    done();
+                });
+            });
+
+            it('should pass translated string as a second argument', function(done) {
+                G.string('hello world', function(error, translation) {
+                    expect(translation).to.be.a('string')
+
+                    done();
+                });
+            });
+        });
+
+        context('when API call fails', function() {
+            beforeEach(function() {
+                this.post.reply(400);
+            });
+
+            it('should pass an error object', function(done) {
+                G.string('hello world', function(error, translation) {
+                    expect(error).to.be.an(Error);
+
+                    done();
+                });
+            });
+        });
+    });
+
+    describe('website()', function() {
+        beforeEach(function() {
+            this.post = this.mock.post('/tranzizzle.php', {
+                search: 'google.com'
+            });
+        });
+
+        afterEach(function() {
+            delete this.post;
+        });
+
+        context('when API call succeeds', function() {
+            before(function() {
+                this.responseText = fs.readFileSync(__dirname + '/mocks/textilizer_success.html');
+            });
+
+            after(function() {
+                delete this.responseText;
+            });
+
+            beforeEach(function() {
+                this.post.reply(200, this.responseText);
+            });
+
+            it('should pass null as error', function(done) {
+                G.website('google.com', function(error, translation) {
+                    expect(error).to.be(null);
+
+                    done();
+                });
+            });
+
+            it('should pass translated string as a second argument', function(done) {
+                G.website('google.com', function(error, translation) {
+                    expect(translation).to.be.a('string')
+
+                    done();
+                });
+            });
+        });
+
+        context('when API call fails', function() {
+            beforeEach(function() {
+                this.post.reply(400);
+            });
+
+            it('should pass an error object', function(done) {
+                G.website('google.com', function(error, translation) {
+                    expect(error).to.be.an(Error);
+
+                    done();
+                });
+            });
+        });
+    });
 });
